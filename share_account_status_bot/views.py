6f9e19a2b2284e72c0030cd11b9af60868816ade
aShangxuanYang <shang-xuan.yang@access-company.com>
2022-05-14 16:39:53 +0800

    [ADD] create_or_retrieve_user function

--- share_account_status_bot/views.py
+++ share_account_status_bot/views.py
@@ -1,17 +1,15 @@
 from os import environ
 
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 
+from share_account_status_bot.models import Service, User
+
 import logging
-import requests
-import json
 
 # Create your views here.
 
@@ -38,13 +36,21 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):  # 如果有訊息事件
-                display_name = get_user_name(event.source.user_id)
+                user = create_or_retrieve_user(event.source.user_id)
+                user_id, display_name = user.user_id, user.name
 
-                logging.debug('UserId is: ' + event.source.user_id)
+                logging.debug(event.message)
                 logging.debug('Text is: ' + event.message.text)
                 logging.debug('Name is: ' + display_name)
 
-                text = 'Hi ' + display_name + "\n" + event.message.text
+                switch = {
+                    'go': lambda: online(user_id),
+                    'stop': lambda: offline(user_id),
+                    'search': lambda: search(),
+                }
+
+                text = switch.get(event.message.text, help)()
+
                 line_bot_api.reply_message(  # 回復傳入的訊息文字
                     event.reply_token,
                     TextSendMessage(text=text)
@@ -53,10 +59,52 @@ def callback(request):
     else:
         return HttpResponseBadRequest()
 
-def get_user_name(user_id):
-    header = {'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN}
-    response = requests.get('https://api.line.me/v2/bot/profile/' + user_id, headers = header)
+def get_user_name(id):
+    return line_bot_api.get_profile(id).display_name
 
-    logging.debug("User Profile: " + str(response.json()))
+def create_or_retrieve_user(id):
+    logging.debug('UserId is: ' + id)
 
-    return response.json()['displayName']
+    try:
+        user = User.objects.get(user_id=id)
+    except User.DoesNotExist:
+        logging.debug("start to create a new user.....")
+        display_name = get_user_name(id)
+        user = User(user_id=id, name=display_name)
+        user.save()
+
+    logging.debug('User ' + str(user))
+    return user
+
+def online(user):
+    # user_service = UserService.objects.get(user_id=user.user_id)
+    # user_service.is_online = True
+    # user_service.save()
+    return user.name + " online."
+
+def offline(user):
+    # user_service = UserService.objects.get(account='k19980506')
+    # user_service.is_online = False
+    # user_service.save()
+    return user.name + " offline."
+
+def search():
+    # s = Service.objects.get(account='k19980506')
+    # logging.debug('Search:' + serializers.serialize('json', [s]))
+    # return "online" if s.is_online else "offline"
+    return "search"
+
+def help():
+    return """
+    Thanks for use this service.
+
+    key:
+        go + 'streaming service name': change your account status to online.
+        stop + 'streaming service name': change your account status to offline.
+        search: check the account status.
+
+    e.g.:
+        go kkbox
+        stop netflix
+        search
+    """
