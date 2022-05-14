d0a75549ca331386ae1bc8a3cc35da745e9c008a
aShangxuanYang <shang-xuan.yang@access-company.com>
2022-05-14 14:23:30 +0800

    [UPDATE] db

---
+++ share_account_status_bot/management/commands/seed.py
@@ -0,0 +1,59 @@
+import logging
+from django.core.management.base import BaseCommand
+
+from share_account_status_bot.models import Service, User, UserService
+
+
+""" Clear all data and creates dummy data """
+MODE_REFRESH = 'refresh'
+
+""" Clear all data and do not create any object """
+MODE_CLEAR = 'clear'
+
+class Command(BaseCommand):
+		help = "seed database for testing and development."
+
+		def add_arguments(self, parser):
+				parser.add_argument('--mode', type=str, help="Mode")
+
+		def handle(self, *args, **options):
+				self.stdout.write('seeding data...')
+				run_seed(self, options['mode'])
+				self.stdout.write('done.')
+
+
+def clear_data():
+		"""Deletes all the table data"""
+		logging.info("Delete instances")
+		User.objects.all().delete()
+		Service.objects.all().delete()
+		UserService.objects.all().delete()
+
+
+def create_data():
+		logging.info("Creating data")
+
+		service = Service(name='kkbox')
+		service.save()
+		logging.info("{} service created.".format(service))
+		user = User(id='12345678', name='King')
+		user.save()
+		logging.info("{} user created.".format(user))
+		user_service = UserService(user=user, service=service, account='k19980506')
+		user_service.save()
+		logging.info("{} user created.".format(user_service))
+		return user
+
+def run_seed(self, mode):
+		""" Seed database based on mode
+
+		:param mode: refresh / clear
+		:return:
+		"""
+		# Clear data from tables
+		clear_data()
+		if mode == MODE_CLEAR:
+				return
+
+		# Creating dara
+		create_data()
--- share_account_status_bot/models.py
+++ share_account_status_bot/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+	name = models.CharField(max_length=100)
+	limit = models.IntegerField(default=1)
+
+	# def __str__(self):
+	#     return self.name
+
+class User(models.Model):
+	name = models.CharField(max_length=100)
+	is_admin = models.BooleanField(default=False)
+	services = models.ManyToManyField(Service, through='UserService', through_fields=('user', 'service'))
+
+class UserService(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	service = models.ForeignKey(Service, on_delete=models.CASCADE)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='owner_set')
+	account = models.CharField(max_length=100)
+	is_online = models.BooleanField(default=False)
+	login_devices_count = models.IntegerField(default=0)
+	time = models.TimeField(auto_now_add=True)
